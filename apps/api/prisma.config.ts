f30f733f417a4f275c523d520aa4b8dc575c0943
Kenenisa Mekonnen <mekonnenkenenisa2@gmail.com>
2026-07-26 00:39:34 +0300

    fixed ci issues with prisma url

--- apps/api/prisma.config.ts
+++ apps/api/prisma.config.ts
@@ -1,9 +1,14 @@
 import { config } from "dotenv";
 import { resolve } from "node:path";
-
+import { existsSync } from "node:fs";
 import { defineConfig, env } from "prisma/config";
 
-config({ path: resolve(import.meta.dirname, "../../.env") });
+// config({ path: resolve(import.meta.dirname, "../../.env") });
+const envPath = resolve(import.meta.dirname, "../../.env");
+
+if (existsSync(envPath)) {
+  config({ path: envPath });
+}
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
